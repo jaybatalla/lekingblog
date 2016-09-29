c078bbef83f74ddf27613d87fef8fb43718f8c92
jaybatalla <jaybatalla@users.noreply.github.com>
2016-09-29 13:13:16 +0800

    Add files via upload

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  def sample
+  	@controller_message = "Hello From Controller"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  #get 'welcome/index'
+  match ':controller(/:action(/:id))', :via => :get 
+
+  root 'welcome#index'
+end
